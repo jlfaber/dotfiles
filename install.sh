9e68a56f478bd793e3bb70f6ad5436ab6774f0f2
Mark Ellzey <mark.thomas@mandiant.com>
2010-09-28 15:29:10 -0400

    Adding more installer checks

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ then
    cp dot.author ~/.author
 fi
 
-#cp ~/.vimrc ~/.vimrc.bak
-#cp ./dot.vimrc ~/.vimrc
-#mkdir ~/.vim
-#cp -R ./dot.vim ~/.vim 
-# cp dot.gdbinit ~/.gdbinit
+cp ~/.vimrc ~/.vimrc.bak
+cp ./dot.vimrc ~/.vimrc
+mkdir ~/.vim
+cp -R ./dot.vim ~/.vim 
+
+if [ -f "~/.gdbinit" ]
+then
+   cp dot.gdbinit ~/.gdbinit
+fi
